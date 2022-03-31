0278b17cb52c3bb1f9791afc844d1723c4cb271f
Jade Bilkey <herself@thefumon.com>
2022-03-30 22:18:23 -0400

    Working Object Oriented Saving

--- filesUI.py
+++ filesUI.py
@@ -2,62 +2,40 @@ import tkinter as tk
 import json
 from tkinter import messagebox
 from utility import find_files
-from datetime import datetime
+from datetime import datetime, timezone
 import os
-import time
 
 # ---------------------------- function field ------------------------------- #
 
 # ---------------------------- search File Function ------------------------------- #
 
 # ---------------------------- Save Function------------------------------- #
-def saveToJson():
+def saveToJson(fname: str, pathname: str, searchResult: list[str]) -> None:
     # This function is not included in the FindFilesWindow class for a good reason
     # That reason is to try and isolate code to one responsibility
     # The FindFilesWindow class is responsible for interacting with the user and checking whether input is valid
     # This function is responsible for performing the actual action itself and takes arguments it knows are already valid
 
-    # date time properties needs to use the json.dumps to transfer to the json format(serialize)
-    funcName = "find_files"
-    fnameInput = fileEntry.get()
-    fpathInput = pathEntry.get()
-    searchResult = text.get('1.0', END)
-    recordDate = json.dumps(datetime.now().strftime('%Y-%m-%d'))
+    with open("json/searchFiles.json", "a+") as data_file:
+        fileLength = data_file.tell()
 
-    new_data = {
-        funcName: {
-            "fname": fnameInput,
-            "pathname": fpathInput,
-            "searchResult": searchResult,
-            "recordDate": recordDate
-        }
-    }
-    # Todo:  add a check for regex
-    if len(fnameInput) == 0 or len(fpathInput) == 0 or len(searchResult) == 0:
-        messagebox.showinfo(
-            title="Oops", message="Please make sure you haven't left any fields empty.")
-    else:
-        try:
-            with open("json/searchFiles.json", "r") as data_file:
-                # Reading old data
-                data = json.load(data_file)
-        except FileNotFoundError:
-            with open("json/searchFiles.json", "w") as data_file:
-                json.dump(new_data, data_file, indent=4)
+        if fileLength:
+            # There's data in the file. Read it
+            data_file.seek(0)
+            data = json.load(data_file)
+            # Reset to start to prepare for write
+            data_file.seek(0)
+            data_file.truncate()
         else:
-            # Updating old data with new data
-            data.update(new_data)
-
-            with open("json/searchFiles.json", "w") as data_file:
-                # Saving updated data
-                json.dump(data, data_file, indent=4)
-        finally:
-            #make sure the program runs more smoothly
-            time.sleep(0.5)
-            fileEntry.delete(0, END)
-            pathEntry.delete(0, END)
-            text.delete('1.0', END)
-
+            data = {}
+    
+        # Updating old data with new data
+        data[datetime.now(timezone.utc).isoformat()] = {
+                "fname": fname,
+                "pathname":  pathname,
+                "searchResult":  searchResult
+            }
+        json.dump(data, data_file, indent=4)
 
 # ----------------------------Delete File Funtion------------------------------- #
 def delete():
@@ -73,8 +51,11 @@ def delete():
 # ---------------------------- UI SETUP ------------------------------- #
 
 class FindFilesWindow:
+    currentSearchResult: list[str]
+
     def __init__(self, parent) -> None:
         self.frame = tk.Frame(parent, padx=35, pady=35)
+        self.frame.pack(side="top")
         # Create interface
 
         # Labels
@@ -114,17 +95,36 @@ class FindFilesWindow:
 
 
         # Text
+        self.currentSearchResult = None
         self.text = tk.Text(self.frame, height=5, width=30)
         self.text.grid(row=3, column=1)
 
     def searchFile(self) -> None:
         fnameInput = self.fileEntry.get()
         fpathInput = self.pathEntry.get()
-        searchResult = find_files(fnameInput, fpathInput)
-        self.text.insert(tk.END, f"{searchResult}")
+        self.currentSearchResult = find_files(fnameInput, fpathInput)
+        self.text.insert(tk.END, self.currentSearchResult)
     
     def save(self) -> None:
+        # DO INPUT VALIDATION HERE
+        if self.currentSearchResult is None:
+            messagebox.showinfo(title="Oops", message="Need to search before saving")
+            # Exit early
+            return
         
+        fnameInput = self.fileEntry.get()
+        fpathInput = self.pathEntry.get()
+
+        saveToJson(fnameInput, fpathInput, self.currentSearchResult)
+
+        # Clear the inputs
+        self.fileEntry.delete(0, tk.END)
+        self.pathEntry.delete(0, tk.END)
+        self.text.delete("1.0", tk.END)
+        self.currentSearchResult = None
+    
+    def delete(self) -> None:
+        pass
 
 
 class MainWindow:
@@ -135,12 +135,14 @@ class MainWindow:
 
         # Create a root object for the rest of the items
         self.frame = tk.Frame(parent)
+        self.frame.pack(fill="both")
 
         # Create buttons
         self.findFilesButton = tk.Button(
             self.frame,
             text='Find Files',
-            command=self.gotoFindFiles)
+            command=self.makeFindFiles)
+        self.findFilesButton.pack(fill="both")
 
     def makeCommandWindow(self) -> tk.Toplevel:
         newWindow = tk.Toplevel(self.parent)
@@ -152,14 +154,10 @@ class MainWindow:
         newParent.title("FindFiles Function")
         self.childCommandInstances.append(FindFilesWindow(newParent))
 
-
-
-
-
 if __name__ == "__main__":
     #Global Config
     root = tk.Tk()
-    MainWindow(root)
+    window = MainWindow(root)
     root.mainloop()
 
 
